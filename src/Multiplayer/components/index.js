77b0b808d5859acbc22abb86c8fbe8d9950c629b
jerico <jericosalvador@noreply.github.com>
2021-03-07 18:37:55 -0800

    removed some unused files

--- src/Multiplayer/components/index.js
+++ src/Multiplayer/components/index.js
@@ -1,5 +1,3 @@
-
-export {default as Game} from './Game'
 export {default as board, boardstates} from './Board'
 export {default as RoomSelection} from './RoomSelection'
 export {default as PageSelector} from './PageSelector'
